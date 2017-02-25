011b1d6404d65ab55b932840d045f6a6fbb925fa
Karan <karangoel16@yahoo.com>
2017-02-25 04:30:19 -0500

    twitter initial structure prep

--- codes/twitter.py
+++ codes/twitter.py
@@ -1,22 +1,37 @@
 import tweepy
+from tweepy.streaming import StreamListener
+from tweepy import Stream
 import csv
 import configparser as cp
+import json
+'''
+Global declaration of variables
+'''
+Config = cp.ConfigParser()
+DirName='/home/karan/Documents/GIT_HUB/BIG_DATA'
+Config.read(DirName+"/Database/config.ini");
+C_key=Config.get('Twitter','C_key');
+C_token=Config.get('Twitter','C_token');
+A_key=Config.get('Twitter','A_key');
+A_token=Config.get('Twitter','A_token');
+auth=tweepy.OAuthHandler(C_key,C_token);
+auth.set_access_token(A_key,A_token);
+api=tweepy.API(auth);
+class bot_stream(StreamListener):
+    def post(self,username,tid):
+        api.update_status('@'+username+' i am yet to begin giving answers!!!!',in_reply_to_status_id=tid);
+    def on_data(self,data):
+        json_load = json.loads(data)
+        #print(json_load['text']);
+        user_data=json_load['user']
+        self.post( user_data['screen_name'],json_load['id']);
+    def on_error(self,status):
+        print(status)
 class bot_handler:
-    def __init__(self,DirName):
-        Config = cp.ConfigParser()
-        Config.read(DirName+"/Database/config.ini");
-        self.C_token=Config.get('Twitter','C_token');
-        self.C_key=Config.get('Twitter','C_key');
-        self.A_key=Config.get('Twitter','A_key');
-        self.A_token=Config.get('Twitter','A_token');
-        print(self.A_token);
-        self.auth=tweepy.OAuthHandler(self.C_key,self.C_token);
-        self.auth.set_access_token(self.A_key,self.A_token);
-        self.api=tweepy.API(self.auth);
-    def post(self,string):
-        self.api.update_status(string);
-    def get_last_tweet(self):
-        tweet = self.api.user_timeline(id = self.client_id, count = 1)[0]
-t=bot_handler('/home/karan/Documents/GIT_HUB/BIG_DATA');
-t.post("test tweet");
+    def __init__(self):
+        l=bot_stream();
+        stream = tweepy.Stream(auth, l)
+        stream.filter(track=['@SmartGatorAI'])
+t=bot_handler();
+#t.post("test tweet");
 

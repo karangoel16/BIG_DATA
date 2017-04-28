ea6a31f6d6f042b1814120177abb9a0aaaa7c517
vishalkg <vishal.gupta4081@gmail.com>
2017-04-28 12:40:22 -0400

    code cleaned and comments completed

--- codes/model.py
+++ codes/model.py
@@ -74,9 +74,10 @@ class RNNModel:
         elif self.device is None:
             return None
         else:
-            print('Warning: Error detected in device name: {}, switch to default device'.format(self.device))
+            print('Warning: Error detected in device name: {}, switching to default device'.format(self.device))
             return None
 
+    # Define network configuration, loss function and optimizer #
     def build_network(self):
         outputProjection = None
         # Sampled softmax only makes sense if we sample less than vocabulary size.
@@ -106,60 +107,54 @@ class RNNModel:
                         self.textdata.vocab_size()),
                     self.dtype)
 
-        # All the model params are initialized on CPU memory by default
-        # set this to self.device() if you want this also on GPU memory
+        # All the model params are initialized on CPU memory by default #
+        # set this to self.device() if you want this also on GPU memory #
         with tf.device('/cpu:0'):
             enc_dec_cell = tf.contrib.rnn.BasicLSTMCell(self.hiddenSize,
                                                     state_is_tuple=True)
-            if not self.test:  # TODO: Should use a placeholder instead
-                enc_dec_cell = tf.contrib.rnn.DropoutWrapper(enc_dec_cell,
-                                                         input_keep_prob=1.0,
-                                                         output_keep_prob=self.dropout)
+            if not self.test:
+                enc_dec_cell = tf.contrib.rnn.DropoutWrapper(enc_dec_cell,input_keep_prob=1.0,output_keep_prob=self.dropout)
+            
             enc_dec_cell = tf.contrib.rnn.MultiRNNCell([enc_dec_cell] * self.numLayers,state_is_tuple=True)
-
             self.encoder  = [tf.placeholder(tf.int32, [None, ]) for _ in range(self.maxLenEnco)]
-
-            #TODO:- check if operation name is not necessary for placeholders
             self.decoder  = [tf.placeholder(tf.int32,[None, ],name='inputs') for _ in range(self.maxLenDeco)]
             self.decoder_weights=[tf.placeholder(tf.float32,[None,],name='weights') for _ in range(self.maxLenDeco)];
             self.decoder_targets  = [tf.placeholder(tf.int32, [None, ],name='targets') for _ in range(self.maxLenDeco)]
 
             if self.attention:
-                print("running attention mechanism")
-                decoder_outputs, states = tf.contrib.legacy_seq2seq.embedding_attention_seq2seq(
-                    self.encoder,  # List<[batch=?, inputDim=1]>, list of size args.maxLength
-                    self.decoder,  # For training, we force the correct output (feed_previous=False)
-                    enc_dec_cell,
-                    self.textdata.vocab_size(),
-                    self.textdata.vocab_size(),  # Both encoder and decoder have the same number of class
-                    embedding_size=self.embeddingSize,  # Dimension of each word
-                    output_projection=outputProjection.getWeights() if outputProjection else None,
-                    feed_previous=bool(self.test)
+                print("Running attention mechanism")
+                decoder_outputs, states = tf.contrib.legacy_seq2seq.embedding_attention_seq2seq (
+                        self.encoder,  # List<[batch=?, inputDim=1]>, list of size args.maxLength
+                        self.decoder,  # For training, we force the correct output (feed_previous=False)
+                        enc_dec_cell,
+                        self.textdata.vocab_size(),
+                        self.textdata.vocab_size(),  # Both encoder and decoder have the same number of class
+                        embedding_size=self.embeddingSize,  # Dimension of each word
+                        output_projection=outputProjection.getWeights() if outputProjection else None,
+                        feed_previous=bool(self.test)
                     )
             else:
                 print("Running rnn seq-2-seq")
-                decoder_outputs, states = tf.contrib.legacy_seq2seq.embedding_rnn_seq2seq(
-                    self.encoder,  # List<[batch=?, inputDim=1]>, list of size args.maxLength
-                    self.decoder,  # For training, we force the correct output (feed_previous=False)
-                    enc_dec_cell,
-                    self.textdata.vocab_size(),
-                    self.textdata.vocab_size(),  # Both encoder and decoder have the same number of class
-                    embedding_size=self.embeddingSize,  # Dimension of each word
-                    output_projection=outputProjection.getWeights() if outputProjection else None,
-                    feed_previous=bool(self.test)
+                decoder_outputs, states = tf.contrib.legacy_seq2seq.embedding_rnn_seq2seq (
+                        self.encoder,  # List<[batch=?, inputDim=1]>, list of size args.maxLength
+                        self.decoder,  # For training, we force the correct output (feed_previous=False)
+                        enc_dec_cell,
+                        self.textdata.vocab_size(),
+                        self.textdata.vocab_size(),  # Both encoder and decoder have the same number of class
+                        embedding_size=self.embeddingSize,  # Dimension of each word
+                        output_projection=outputProjection.getWeights() if outputProjection else None,
+                        feed_previous=bool(self.test)
                     )
-            #print(self.test)
 
         if self.test:
-            #print(decoder_outputs)
             if not outputProjection:
                 self.outputs = decoder_outputs
             else:
                 self.outputs = [outputProjection(output) for output in decoder_outputs]
         
         else:
-            #this is when we are not testing on our model but train our system
-            # set device (gpu) in Config.ini
+            # Define loss function, optimizer                               #
+            # Customize device/gpu for gradient calculation in Config.ini   #
             with tf.device(self.get_device()):
                 self.loss_fct = tf.contrib.legacy_seq2seq.sequence_loss(
                     decoder_outputs,
@@ -178,11 +173,13 @@ class RNNModel:
                 )
                 self.opt_op = opt.minimize(self.loss_fct);
 
+    # Defines how input is fed into network at each step #
     def step(self, batch):
         feed_dict = {}
         ops = None
-        #TODO:- Remove args dependecy if possible
-        if not self.test:  # Training
+
+        # Training Phase #
+        if not self.test:
             feed_dict = {self.encoder[i]: batch.var_encoder[i]
                          for i in range(self.maxLenEnco)}
             feed_dict.update({self.decoder[i]: batch.var_decoder[i]
@@ -192,10 +189,13 @@ class RNNModel:
             feed_dict.update({self.decoder_weights[i]: batch.var_weight[i]
                               for i in range(self.maxLenDeco)})
             ops = (self.opt_op, self.loss_fct)
-        else:  # Testing (batchSize == 1)
+        
+        # Testing phase (equivalent to batchSize == 1) #
+        else:
             feed_dict = {self.encoder[i]: batch.var_encoder[i]
                          for i in range(self.maxLenEnco)}
             feed_dict[self.decoder[0]]  = [self.textdata.var_token]
             ops = tuple([self.outputs])
-        # Return one pass operator
+        
+        # Return one pass operator #
         return ops, feed_dict
